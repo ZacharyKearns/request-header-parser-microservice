c8cecf4e80cd4a553461630366917f976fac5a2d
Zachary Kearns <zach_kearns89@live.com>
2016-08-25 21:09:26 -0400

    unmix content

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var os = window.navigator.userAgent.match(/\(([^)]+)\)/);
-  $.get("http://ipinfo.io", function(response) {
+  $.get("//ipinfo.io", function(response) {
     $('#output').text(
       '{ "ipaddress":' + '"' + response.ip + '"' +
       ', "language":' + '"' + window.navigator.languages[0] + '"' +
