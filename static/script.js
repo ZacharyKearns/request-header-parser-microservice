13b0bb2acb9b5a5372df9ced9c162a5ffb260eab
Zachary Kearns <zach_kearns89@live.com>
2016-08-29 21:31:22 -0400

    update window nav

--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   var os = window.navigator.userAgent.match(/\(([^)]+)\)/);
+  var language = window.navigator.languages ? window.navigator.languages[0] : window.navigator.language;
   $.get("https://api.ipify.org?format=jsonp", function(res) {
     $('#output').text(
       '{ "ipaddress":' + '"' + res.ip + '"' +
-      ', "language":' + '"' + window.navigator.languages[0] + '"' +
+      ', "language":' + '"' + language + '"' +
       ',"software":' + '"' + os[1] + '"' + ' }'
     );
   }, "jsonp");
